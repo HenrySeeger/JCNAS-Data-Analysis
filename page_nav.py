0b1bc287f1d3d32598822f7d12038459321d7987
Henry Seeger <hiseeger@gmail.com>
2026-07-14 12:48:22 -0400

    Set up all files, linked pages via page_nav.py. Functions handling resets or other events that should occur when switching to or from certain pages will have to be set up later.

--- page_nav.py
+++ page_nav.py
@@ -1,11 +1,10 @@
 import streamlit as st
 
-pages = {
-    "Resources": [
-        st.Page("home.py", title="Home"),
-        st.Page("graphing.py", title="Graphing"),
-    ],
-}
+pages = [st.Page("home.py", title = "Home"),
+         st.Page("data_obj.py", title = "Data Objects Creation and Management"),
+         st.Page("graphing.py", title = "Graphing"),
+         st.Page("stats.py", title = "Statistics"),
+         st.Page("faq.py", title = "FAQ")]
 
 pg = st.navigation(pages)
 pg.run()
