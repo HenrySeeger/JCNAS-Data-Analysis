1ee170a818873e43ed5543d5665ad8221667f4e5
Henry Seeger <hiseeger@gmail.com>
2026-07-16 21:47:04 -0400

    Changed how the master data is stored (now as DataObject). Both master files now required for other actions, removing one/both post-upload locks everything down. User gets feedback when they attempt to create a new DataObject (distinct for success and failure). SOlved data viewer issues, now in working condition.

--- data_obj.py
+++ data_obj.py
@@ -5,24 +5,22 @@ import streamlit as st
 import DataObject as do
 import matplotlib.pyplot as plt
 
-st.session_state.setdefault("app_data_master", {"name" : None, "data" : None})
-st.session_state.setdefault("res_data_master", {"name" : None, "data" : None})
-st.session_state.setdefault("data_objects", {})
+st.session_state.setdefault("master_data", None)
+st.session_state.setdefault("data_objects", [])
 
 st.header("Managing Data Objects")
 
 upload_datasets_expander = st.expander(label = "Upload Datasets")
 
 with upload_datasets_expander:
-  applications_entry = st.file_uploader(label = "Upload the applications dataset", max_upload_size = 2000)
-  if applications_entry is not None and st.session_state.app_data_master["name"] is not applications_entry.name:
-      st.session_state.app_data_master["name"] = applications_entry.name
-      st.session_state.app_data_master["data"] = pd.read_csv(applications_entry)
+  def master_data_uploaders_on_change():
+    if (st.session_state.applications_entry is not None and st.session_state.responses_entry is not None):
+      st.session_state.master_data = do.DataObject("Master", pd.read_csv(st.session_state.applications_entry), pd.read_csv(st.session_state.responses_entry))
+    else:
+      st.session_state.master_data = None
   
-  responses_entry = st.file_uploader(label = "Upload the responses dataset", max_upload_size = 2000)
-  if responses_entry is not None and st.session_state.res_data_master["name"] is not responses_entry.name:
-      st.session_state.res_data_master["name"] = responses_entry.name
-      st.session_state.res_data_master["data"] = pd.read_csv(responses_entry)
+  applications_entry = st.file_uploader(label = "Upload the applications dataset", max_upload_size = 2000, on_change = master_data_uploaders_on_change, key = "applications_entry") 
+  responses_entry = st.file_uploader(label = "Upload the responses dataset", max_upload_size = 2000, on_change = master_data_uploaders_on_change, key = "responses_entry")
 
 creation_expander = st.expander(label = "Create a Data Object", key = "DataObjectExpander")
 
@@ -30,10 +28,14 @@ with creation_expander:
   creation_name_input = st.text_input(label = "Name", key = "DataObjectName", placeholder = "Enter data object name", disabled = applications_entry is None or responses_entry is None)
   
   def creation_button_on_click():
-    if creation_name_input not in st.session_state.data_objects.keys():
-      st.session_state.data_objects[creation_name_input] = do.DataObject(creation_name_input, copy.deepcopy(st.session_state.app_data_master), copy.deepcopy(st.session_state.res_data_master))
+    st.session_state.DataObjectName = ""
+    if creation_name_input not in [obj.name for obj in st.session_state.data_objects]:
+      st.session_state.data_objects.append(do.DataObject.from_dataobject(creation_name_input, st.session_state.master_data))
+      st.toast(body = f"DataObject '{creation_name_input}' successfully created")
+    else:
+      st.toast(body = f"'{creation_name_input}' is the name of an existing DataObject")
 
-  st.button(label = "Create Object", disabled = applications_entry is None or responses_entry is None, on_click = creation_button_on_click)
+  obj_creation_button = st.button(label = "Create Object", disabled = applications_entry is None or responses_entry is None, on_click = creation_button_on_click)
 
 data_view_expander = st.expander("View Datasets", key = "DataViewer")
 
@@ -44,6 +46,12 @@ with data_view_expander:
     else:
       return data["name"][:data["name"].rfind(".")]
   
-  data_viewer_selection = st.selectbox(label = "Select a dataset to view", options = ["No Selection"] + [data for data in [st.session_state.app_data_master, st.session_state.res_data_master] if data["name"] is not None], format_func = data_viewer_formatting, key = "DataViewerSelecter", disabled = applications_entry is None and responses_entry is None)
-  if data_viewer_selection != "No Selection":
-    st.dataframe(data_viewer_selection["data"])
+  if st.session_state.master_data is not None:
+    data_viewer_selection = st.selectbox(label = "Select a dataset to view", 
+                                        options = ["No Selection"] + [data for obj in st.session_state.data_objects + [st.session_state.master_data] for data in [{"name" : f"{obj.name}: applications", "data" : obj.applications}, {"name" : f"{obj.name}: responses", "data" : obj.responses}]],
+                                        format_func = data_viewer_formatting, key = "DataViewerSelecter",
+                                        disabled = applications_entry is None and responses_entry is None)
+    if data_viewer_selection != "No Selection":
+      st.dataframe(data_viewer_selection["data"])
+  else:
+    st.text("Both an applications dataset and a responses dataset are required to be uploaded before any datsetsa can be viewed")
